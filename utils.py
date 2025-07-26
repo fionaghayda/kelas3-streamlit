5de8365c673fc61382d2ca960124961e31fe484d
fionaghayda <fionaghayda2017@gmail.com>
2025-07-26 12:26:57 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,17 +1,58 @@
 import streamlit as st
 
-def check_password():
-    def password_entered():
-        if st.session_state["password"] == "kelas3ku":
-            st.session_state["password_correct"] = True
-        else:
-            st.session_state["password_correct"] = False
+# Fungsi autentikasi berdasarkan halaman
 
-    if "password_correct" not in st.session_state:
-        st.text_input("Masukkan Password", type="password", on_change=password_entered, key="password")
+def check_password_kelas():
+    def password_entered():
+        if st.session_state["password_kelas"] == st.secrets["password_kelas"]:
+            st.session_state["password_correct_kelas"] = True
+            del st.session_state["password_kelas"]
+        else:
+            st.session_state["password_correct_kelas"] = False
+
+    if "password_correct_kelas" not in st.session_state:
+        st.text_input(
+            "Masukkan password untuk akses Data Siswa dan Nilai:",
+            type="password",
+            on_change=password_entered,
+            key="password_kelas",
+        )
         return False
-    elif not st.session_state["password_correct"]:
-        st.text_input("Masukkan Password", type="password", on_change=password_entered, key="password")
+    elif not st.session_state["password_correct_kelas"]:
+        st.text_input(
+            "Masukkan password untuk akses Data Siswa dan Nilai:",
+            type="password",
+            on_change=password_entered,
+            key="password_kelas",
+        )
+        st.error("Password salah.")
+        return False
+    else:
+        return True
+
+def check_password_galeri():
+    def password_entered():
+        if st.session_state["password_galeri"] == st.secrets["password_galeri"]:
+            st.session_state["password_correct_galeri"] = True
+            del st.session_state["password_galeri"]
+        else:
+            st.session_state["password_correct_galeri"] = False
+
+    if "password_correct_galeri" not in st.session_state:
+        st.text_input(
+            "Masukkan password untuk akses Galeri dan Komentar:",
+            type="password",
+            on_change=password_entered,
+            key="password_galeri",
+        )
+        return False
+    elif not st.session_state["password_correct_galeri"]:
+        st.text_input(
+            "Masukkan password untuk akses Galeri dan Komentar:",
+            type="password",
+            on_change=password_entered,
+            key="password_galeri",
+        )
         st.error("Password salah.")
         return False
     else:
